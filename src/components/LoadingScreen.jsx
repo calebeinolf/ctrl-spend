1209fc116390d797b558d999a3f1b6542cb69663
calebeinolf <calebeinolf@gmail.com>
2025-08-08 17:39:23 -0400

    change loading screen to app icon

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,15 +1,10 @@
+import Logo from "../assets/ctrlspend_favicon2.svg";
+
 const LoadingScreen = () => {
   return (
     <div className="min-h-screen md-surface flex items-center justify-center">
       <div className="text-center">
-        <div className="mx-auto mb-4 w-12 h-12 md-spinner"></div>
-        <h2 className="md-headline-small mb-2">Ctrl+Spend</h2>
-        <p
-          className="md-body-large"
-          style={{ color: "var(--md-on-surface-variant)" }}
-        >
-          Loading...
-        </p>
+        <img src={Logo} alt="Ctrl+Spend" className="mx-auto mb-4 w-60 h-60" />
       </div>
     </div>
   );
