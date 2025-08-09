6cb7791d392b72b25f6acee042b45212cdc49169
calebeinolf <calebeinolf@gmail.com>
2025-08-09 18:14:15 -0400

    logo position on loading screen

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -7,7 +7,7 @@ const LoadingScreen = () => {
         <img
           src={Logo}
           alt="Ctrl+Spend"
-          className="mx-auto mb-4 w-60 h-60 opacity-80 translate-y-[3px]"
+          className="mx-auto mb-4 w-60 h-60 opacity-80 translate-y-[10px]"
         />
         {/* <div className="w-6 h-6 border-2 border-lime-600 border-t-transparent rounded-full animate-spin mx-auto"></div> */}
       </div>
